1f043fc8675538842077d38d4850257507403218
4220182 <4220182@qq.com>
2017-07-03 22:36:13 +0800

    Create remove-po.sh

---
+++ remove-po.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Generating kubeconfig..."
+export KUBECONFIG="$(pwd)/kubeconfig"
+KUBE_CLUSTER_OPTIONS=--certificate-authority="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+
+ 
+kubectl config set-cluster local-deploy --server="$KUBE_URL" \
+  $KUBE_CLUSTER_OPTIONS
+
+kubectl config set-credentials local-deploy --token=`cat /var/run/secrets/kubernetes.io/serviceaccount/token`
+
+kubectl config set-context local-deploy \
+  --cluster=local-deploy --user=local-deploy \
+  --namespace="$KUBE_NAMESPACE"
+
+kubectl config use-context local-deploy
+
+KILL_PID=`kubectl exec $HOSTNAME -c $KUBE_KILL_NAME -- ps ax |grep java |awk -F" " '{print $1}'`
+
+echo "KILL ${KUBE_KILL_NAME} container KILL_PID: ${KILL_PID}"
+
+kubectl exec $HOSTNAME -c $KUBE_KILL_NAME -- kill -9 $KILL_PID
